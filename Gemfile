1e6538a36425facc678665e15c2dd908a4517bee
Devin Barham <barhamd@gmail.com>
2015-05-18 00:58:20 -0600

    Editable homepage

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+
 gem 'haml'
+gem 'data_mapper'
+gem 'dm-sqlite-adapter', '~> 1.2.0'
+gem 'pry'
--- myapp.rb
+++ myapp.rb
@@ -3,8 +3,39 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
+require 'data_mapper'
+require 'pry'
 
-get '/' do
-  haml :index
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/blog.db")
+
+class Page
+  include DataMapper::Resource
+  property :id,         Serial
+  property :html,       Text
+  property :css,        Text
+  property :created_at, DateTime
 end
 
+DataMapper.finalize
+
+Page.auto_upgrade!
+
+get '/' do
+  @html = Page.last.html
+  erb :index
+end
+
+get '/edit' do
+  @page = Page.last
+  erb :edit
+end
+
+post '/' do
+  @page = Page.new(html: params[:html], created_at: Time.now)
+  if @page.save
+    redirect to '/'
+  else
+    redirect to '/edit'
+  end
+end
