0b498cd25efd8a9934ad83b43f546fbf7ff3c347
Devin Barham <barhamd@gmail.com>
2015-05-23 23:13:35 -0600

    max / min button

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,11 @@ var editor = CodeMirror.fromTextArea(myTextarea, {
   mode:        "htmlembedded",
   theme:       "monokai"
 });
+
+$('#minimize').click(function (e) {
+  $(this).closest('.editor-container').addClass('minimized');
+});
+
+$('#maximize').click(function () {
+  $(this).closest('.editor-container').removeClass('minimized');
+});
