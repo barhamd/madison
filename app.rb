3dc57933002c27a3fd8bf9c55f05207abd4b554f
Devin Barham <barhamd@gmail.com>
2015-05-25 15:19:36 -0600

    Remove unnecessary requires

--- app.rb
+++ app.rb
@@ -1,14 +1,43 @@
 #!/usr/bin/env ruby
 
-require 'rubygems'
-require 'bundler/setup'
-require 'sinatra'
-require 'data_mapper'
-require 'pry'
-
 require_relative 'models/init'
 
 class MyApp < Sinatra::Base
+  enable :session
+  register Sinatra::Flash
+
+  use Warden::Manager do |config|
+    config.serialize_into_session{ |user| user.id }
+    config.serialize_from_session{ |id| User.get(id) }
+
+    config.scope_defaults :default,
+      strategies: [:password],
+      action: 'auth/unauthenticated'
+
+    config.failure_app = self
+  end
+
+  Warden::Manager.before_failure do |env, opts|
+    env['REQUEST_METHOD'] = 'POST'
+  end
+
+  Warden::Strategies.add(:password) do
+    def valid?
+      params['user']['username'] && params['user']['password']
+    end
+
+    def authenticate!
+      user = User.first(username: params['user']['username'])
+
+      if user.nil?
+        fail!("The username you entered does not exist.")
+      elsif user.authenticate(params['user']['password'])
+        success!(user)
+      else
+        fail!("Could not log in")
+      end
+    end
+  end
 
   get '/' do
     @html = Page.last.html
