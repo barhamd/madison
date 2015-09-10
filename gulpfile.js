98b11c27f08924d5624b86f17afb1e9cc6f3cc9d
Devin Barham <barhamd@gmail.com>
2015-09-10 10:36:22 -0600

    Concat and minify js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,18 @@
 // Include gulp
 var gulp = require('gulp');
+// Include plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+// Define default destination folder
+var dest = 'public/';
+// Concatenate & Minify JS Files
+gulp.task('scripts', function() {
+  return gulp.src('assets/javascripts/*.js')
+    .pipe(concat('main.js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest(dest + 'js'));
+});
+// Default Task
+gulp.task('default', ['scripts']);
